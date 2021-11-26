46e4ee89ffa59bc9714b3079373254701cc42227
John George Francis <arbitrandomuser@gmail.com>
2021-11-26 14:59:51 +0530

    day 8 second part

--- 2019/day8/start.jl
+++ 2019/day8/start.jl
@@ -1,3 +1,4 @@
+using Plots 
 f = open("input")
 lines = readlines(f)
 img = lines[1]
@@ -19,4 +20,29 @@ answer = count("1",layers[minlayer])*count("2",layers[minlayer])
 println("first answer : $(answer)")
 end
 
+#canvas = reshape([2 for i in 1:layersize],(6,25))
+function secondpart()
+  canvas = ['2' for i in 1:layersize]
+  for layer in layers
+    for (i,c) in enumerate(layer)
+      if canvas[i]=='2'
+        canvas[i] = c
+      else
+        nothing
+      end
+    end
+  end
+  x = []
+  y = []
+  for (i,c) in enumerate(canvas)
+    if c=='1'
+      push!(x,i%25)
+      push!(y,i÷25)
+    end
+  end
+  plt = scatter(x,-y,width=50)
+  savefig(plt,"secondanswer.png")
+  println("open secondanswer.png for second answer")
+end
 firstpart()
+secondpart()
